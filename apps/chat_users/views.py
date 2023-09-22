73e441ebc0c7c893154d8c87d59597f2596a0cf6
Bhavani M <bhavani.lingaraju@gmail.com>
2023-09-22 13:31:32 +0200

    Made small changes in views

--- apps/chat_users/views.py
+++ apps/chat_users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import User
+
 # Create your views here.
+
+def user_list():
+    my_users = User.objects.all()
+    
+    context = {'my_users': my_users}
+    
+    return render()
